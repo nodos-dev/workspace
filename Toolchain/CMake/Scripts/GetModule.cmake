82095f527af6ef13d8f84c9011b51c5510f17d59
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-05-14 17:27:40 +0300

    CMake now installs module if not found

--- Toolchain/CMake/Scripts/GetModule.cmake
+++ Toolchain/CMake/Scripts/GetModule.cmake
@@ -7,24 +7,35 @@ function(nos_get_module name version out_target_name)
 
     # TODO: Download if not exists.
     if(NOSMAN_EXECUTABLE)
+        # Install module if not exists, silently
+        execute_process(
+            COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NODOS_WORKSPACE_DIR}" install ${name} ${version}
+            RESULT_VARIABLE NOSMAN_RESULT
+            OUTPUT_QUIET
+        )
+        if (NOT NOSMAN_RESULT EQUAL 0)
+            message(FATAL_ERROR "Failed to install ${name} ${version} in workspace")
+        endif()
+
         execute_process(
             COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NODOS_WORKSPACE_DIR}" info ${name} ${version} --relaxed
             RESULT_VARIABLE NOSMAN_RESULT
             OUTPUT_VARIABLE NOSMAN_OUTPUT
         )
 
-        string(REPLACE "." "_" target_name ${name})
-        string(REPLACE "." "_" version_str ${version})
-        string(APPEND target_name "-v${version_str}")
-        string(PREPEND target_name "__nos_generated__")
-        set(${out_target_name} ${target_name} PARENT_SCOPE)
+        if (NOSMAN_RESULT EQUAL 0)
+            string(REPLACE "." "_" target_name ${name})
+            string(REPLACE "." "_" version_str ${version})
+            string(APPEND target_name "-v${version_str}")
+            string(PREPEND target_name "__nos_generated__")
 
-        if(NOSMAN_RESULT EQUAL 0)
             string(STRIP ${NOSMAN_OUTPUT} NOSMAN_OUTPUT)
             # Get "public_include_folder" from JSON output
             string(JSON nos_module_include_folder GET "${NOSMAN_OUTPUT}" "public_include_folder")
             cmake_path(SET ${target_name}_INCLUDE_DIR "${nos_module_include_folder}")
             message(STATUS "Found ${name} ${version} include folder: ${${target_name}_INCLUDE_DIR}")
+
+            set(${out_target_name} ${target_name} PARENT_SCOPE)
 
             if(TARGET ${target_name})
                 message(STATUS "Module ${name}-${version} found in project. Using existing target.")
