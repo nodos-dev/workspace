4902d31af70fae6fd215b9bb14f46f9ebadfe5a4
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-05-20 18:14:12 +0300

    Update subsystem main cpp

--- Toolchain/nosman/data/templates/cpp/subsystem/Source/SubsystemMain.cpp
+++ Toolchain/nosman/data/templates/cpp/subsystem/Source/SubsystemMain.cpp
@@ -7,51 +7,51 @@ NOS_INIT();
 template <typename T>
 T __stdcall Add(T a, T b)
 {
-    return a + b;
+	return a + b;
 }
 
 void __stdcall PrintHelloNodos()
 {
-    nosEngine.LogI("Hello Nodos!");
+	nosEngine.LogI("Hello Nodos!");
 }
+
+static std::unordered_map<uint32_t, MySubsystem*> GExported;
 
 extern "C"
 {
+	nosResult OnRequest(uint32_t minor, void** outSubsystemCtx)
+	{
+		auto it = GExported.find(minor);
+		if (it == GExported.end())
+		{
+			switch (minor)
+			{
+			case 0:
+			{
+				MySubsystem* subsystem = new MySubsystem();
+				subsystem->PrintHelloNodos = PrintHelloNodos;
+				subsystem->Add = Add<int>;
+				GExported[minor] = subsystem;
+				*outSubsystemCtx = subsystem;
+				return NOS_RESULT_SUCCESS;
+			}
+			}
+			return NOS_RESULT_NOT_FOUND;
+		}
+		*outSubsystemCtx = it->second;
+		return NOS_RESULT_SUCCESS;
+	}
 
-static std::unordered_map<uint32_t, const MySubsystem*> GExported;
+	NOSAPI_ATTR nosResult NOSAPI_CALL nosExportSubsystem(nosSubsystemFunctions* subsystemFunctions)
+	{
+		subsystemFunctions->OnRequest = OnRequest;
+		return NOS_RESULT_SUCCESS;
+	}
 
-nosResult OnRequest(uint32_t minor, void** outSubsystemCtx)
-{
-    auto it = GExported.find(minor);
-    if (it == GExported.end())
-    {
-        switch (minor)
-        {
-        case 0:
-        {
-            MySubsystem* subsystem = new MySubsystem();
-            subsystem->PrintHelloNodos = PrintHelloNodos;
-            subsystem->Add = Add<int>;
-            GExported[minor] = subsystem;
-            *outSubsystemCtx = subsystem;
-            return NOS_RESULT_SUCCESS;
-        }
-        return NOS_RESULT_NOT_FOUND;
-    }
-    *outSubsystemCtx = it->second;
-    return NOS_RESULT_SUCCESS;
-}
-
-NOSAPI_ATTR nosResult NOSAPI_CALL nosExportSubsystem(nosSubsystemFunctions* subsystemFunctions)
-{
-    subsystemFunctions->OnRequest = OnRequest;
-    return NOS_RESULT_SUCCESS;
-}
-
-NOSAPI_ATTR nosResult NOSAPI_CALL nosUnloadSubsystem()
-{
-    for (auto& pair : GExported)
-        delete pair.second;
-    return NOS_RESULT_SUCCESS;
-}
+	NOSAPI_ATTR nosResult NOSAPI_CALL nosUnloadSubsystem()
+	{
+		for (auto& pair : GExported)
+			delete pair.second;
+		return NOS_RESULT_SUCCESS;
+	}
 }
