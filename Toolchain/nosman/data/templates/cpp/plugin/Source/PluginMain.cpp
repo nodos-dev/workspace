c4326f5c9d227e7de3723c34ebb5b134497494c5
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-09-03 17:22:51 +0300

    nosman: Update templates to conform to the new API

--- Toolchain/nosman/data/templates/cpp/plugin/Source/PluginMain.cpp
+++ Toolchain/nosman/data/templates/cpp/plugin/Source/PluginMain.cpp
@@ -2,15 +2,23 @@
 #include <Nodos/PluginHelpers.hpp>
 #include <Nodos/Helpers.hpp>
 
-NOS_INIT();
+NOS_INIT()
+NOS_BEGIN_IMPORT_DEPS()
+NOS_END_IMPORT_DEPS()
 
-extern "C"
-{
-NOSAPI_ATTR nosResult NOSAPI_CALL nosExportNodeFunctions(size_t* outCount, nosNodeFunctions** outFunctions)
+nosResult NOSAPI_CALL ExportNodeFunctions(size_t* outCount, nosNodeFunctions** outFunctions)
 {
     *outCount = (size_t)(0);
     if (!outFunctions)
         return NOS_RESULT_SUCCESS;
     return NOS_RESULT_SUCCESS;
+}
+
+extern "C"
+{
+NOSAPI_ATTR nosResult NOSAPI_CALL nosExportPlugin(nosPluginFunctions* out)
+{
+	out->ExportNodeFunctions = ExportNodeFunctions;
+	return NOS_RESULT_SUCCESS;
 }
 }
--- Toolchain/nosman/data/templates/cpp/subsystem/Source/SubsystemMain.cpp
+++ Toolchain/nosman/data/templates/cpp/subsystem/Source/SubsystemMain.cpp
@@ -2,7 +2,9 @@
 #include <mySubsystem/PublicHeader.h>
 #include <Nodos/SubsystemAPI.h>
 
-NOS_INIT();
+NOS_INIT()
+NOS_BEGIN_IMPORT_DEPS()
+NOS_END_IMPORT_DEPS()
 
 template <typename T>
 T __stdcall Add(T a, T b)
@@ -17,41 +19,42 @@ void __stdcall PrintHelloNodos()
 
 static std::unordered_map<uint32_t, MySubsystem*> GExported;
 
+nosResult OnRequest(uint32_t minor, void** outSubsystemCtx)
+{
+    auto it = GExported.find(minor);
+    if (it == GExported.end())
+    {
+        switch (minor)
+        {
+        case 0:
+        {
+            MySubsystem* subsystem = new MySubsystem();
+            subsystem->PrintHelloNodos = PrintHelloNodos;
+            subsystem->Add = Add<int>;
+            GExported[minor] = subsystem;
+            *outSubsystemCtx = subsystem;
+            return NOS_RESULT_SUCCESS;
+        }
+        }
+        return NOS_RESULT_NOT_FOUND;
+    }
+    *outSubsystemCtx = it->second;
+    return NOS_RESULT_SUCCESS;
+}
+
+nosResult NOSAPI_CALL OnPreUnloadSubsystem()
+{
+    for (auto& pair : GExported)
+        delete pair.second;
+    return NOS_RESULT_SUCCESS;
+}
+
 extern "C"
 {
-	nosResult OnRequest(uint32_t minor, void** outSubsystemCtx)
-	{
-		auto it = GExported.find(minor);
-		if (it == GExported.end())
-		{
-			switch (minor)
-			{
-			case 0:
-			{
-				MySubsystem* subsystem = new MySubsystem();
-				subsystem->PrintHelloNodos = PrintHelloNodos;
-				subsystem->Add = Add<int>;
-				GExported[minor] = subsystem;
-				*outSubsystemCtx = subsystem;
-				return NOS_RESULT_SUCCESS;
-			}
-			}
-			return NOS_RESULT_NOT_FOUND;
-		}
-		*outSubsystemCtx = it->second;
-		return NOS_RESULT_SUCCESS;
-	}
-
-	NOSAPI_ATTR nosResult NOSAPI_CALL nosExportSubsystem(nosSubsystemFunctions* subsystemFunctions)
-	{
-		subsystemFunctions->OnRequest = OnRequest;
-		return NOS_RESULT_SUCCESS;
-	}
-
-	NOSAPI_ATTR nosResult NOSAPI_CALL nosUnloadSubsystem()
-	{
-		for (auto& pair : GExported)
-			delete pair.second;
-		return NOS_RESULT_SUCCESS;
-	}
+NOSAPI_ATTR nosResult NOSAPI_CALL nosExportSubsystem(nosSubsystemFunctions* subsystemFunctions)
+{
+    subsystemFunctions->OnRequest = OnRequest;
+    subsystemFunctions->OnPreUnloadSubsystem = OnPreUnloadSubsystem;
+    return NOS_RESULT_SUCCESS;
+}
 }
