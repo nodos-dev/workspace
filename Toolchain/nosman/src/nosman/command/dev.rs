6f343e364b5eaaa694ab7572b42e58c87718d616
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-09-24 16:39:03 +0300

    nosman dev pull: Include engine and workspace folders by default too

--- Toolchain/nosman/src/nosman/command/dev.rs
+++ Toolchain/nosman/src/nosman/command/dev.rs
@@ -10,18 +10,20 @@ pub struct DevPullCommand {
 }
 
 impl DevPullCommand {
-    fn run_dev_pull(&self, modules_dir: PathBuf) -> Result<bool, CommandError> {
+    fn run_dev_pull(&self, dirs: Vec<PathBuf>) -> Result<bool, CommandError> {
         // Scan module folder for git repositories and run "git pull" on them
-        if !modules_dir.exists() {
-            Err (CommandError::InvalidArgumentError { message: format!("No such directory: {}", modules_dir.display()) })
-        } else {
-            let pb = ProgressBar::new_spinner();
-            pb.enable_steady_tick(Duration::from_millis(100));
-            pb.set_message("Scanning for git repositories...");
+        let pb = ProgressBar::new_spinner();
+        pb.enable_steady_tick(Duration::from_millis(100));
+        pb.set_message("Scanning for git repositories...");
+        let mut git_dirs = Vec::new();
+        for dir in dirs {
             let mut stack = Vec::new();
-            stack.push(modules_dir);
-            let mut git_dirs = Vec::new();
+            stack.push(dir);
             while let Some(dir) = stack.pop() {
+                if dir.join(".git").is_dir() {
+                    git_dirs.push(dir);
+                    continue;
+                }
                 for entry in std::fs::read_dir(dir).expect("Failed to read directory") {
                     let entry = entry.expect("Failed to read entry");
                     let path = entry.path();
@@ -32,34 +34,35 @@ impl DevPullCommand {
                     }
                 }
             }
-            pb.set_message("Pulling...");
-            git_dirs.par_iter().for_each(|path| {
-                pb.println(format!("{}{}", "Pulling: ".yellow(), path.display()));
-                let output = std::process::Command::new("git")
-                    .arg("pull")
-                    .current_dir(&path)
-                    .output()
-                    .expect("Failed to run git pull");
-                if !output.status.success() {
-                    pb.println(format!("{}{}{}", "Failed to pull: ".red(), path.display(), String::from_utf8_lossy(&output.stderr)));
-                }
-                // Submodule update recursive
-                let status = std::process::Command::new("git")
-                    .arg("submodule")
-                    .arg("update")
-                    .arg("--init")
-                    .arg("--recursive")
-                    .current_dir(&path)
-                    .status()
-                    .expect("Failed to run git submodule update");
-                if !status.success() {
-                    pb.println(format!("{}{}{}", "Failed to update submodules: ".red(), path.display(), String::from_utf8_lossy(&output.stderr)));
-                }
-                pb.println(format!("{}: {}", path.display().to_string().green(), String::from_utf8_lossy(&output.stdout)));
-            });
-            pb.finish_and_clear();
-            Ok(true)
         }
+        pb.set_message("Pulling...");
+        git_dirs.par_iter().for_each(|path| {
+            pb.println(format!("{}{}", "Pulling: ".yellow(), path.display()));
+            let output = std::process::Command::new("git")
+                .arg("pull")
+                .arg("--autostash")
+                .current_dir(&path)
+                .output()
+                .expect("Failed to run git pull");
+            if !output.status.success() {
+                pb.println(format!("{}{}{}", "Failed to pull: ".red(), path.display(), String::from_utf8_lossy(&output.stderr)));
+            }
+            // Submodule update recursive
+            let status = std::process::Command::new("git")
+                .arg("submodule")
+                .arg("update")
+                .arg("--init")
+                .arg("--recursive")
+                .current_dir(&path)
+                .status()
+                .expect("Failed to run git submodule update");
+            if !status.success() {
+                pb.println(format!("{}{}{}", "Failed to update submodules: ".red(), path.display(), String::from_utf8_lossy(&output.stderr)));
+            }
+            pb.println(format!("{}: {}", path.display().to_string().green(), String::from_utf8_lossy(&output.stdout)));
+        });
+        pb.finish_and_clear();
+        Ok(true)
     }
 }
 
@@ -72,8 +75,9 @@ impl Command for DevPullCommand {
     }
 
     fn run(&self, args: &ArgMatches) -> CommandResult {
-        let modules_dir = PathBuf::from(args.get_one::<String>("modules_dir").unwrap());
-        self.run_dev_pull(modules_dir)
+        let dirs: Vec<&String> = args.get_many::<String>("dir").unwrap_or_default().collect();
+        let dirs: Vec<PathBuf> = dirs.iter().map(|s| PathBuf::from(s)).collect();
+        self.run_dev_pull(dirs)
     }
 
     fn needs_workspace(&self) -> bool {
