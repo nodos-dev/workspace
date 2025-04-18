bc82b658d828408076e428cf9c74675d292fe2b9
Caner Coskun <caner.coskun@nodos.dev>
2025-04-18 18:57:26 +0300

    Conform to Nodos API Changes - Merge plugin & subsystem APIs (#32)

--- Toolchain/CMake/Scripts/FindNodosSDK.cmake
+++ Toolchain/CMake/Scripts/FindNodosSDK.cmake
@@ -50,23 +50,58 @@ macro(nos_find_sdk requested_version out_nos_plugin_sdk out_nos_subsystem_sdk ou
             endforeach()
         endif()
 
-        if(found_version_idx EQUAL -1)
-            message(FATAL_ERROR "No compatible version found.")
-        else()
-            list(GET NOS_VERSIONS ${found_version_idx} found_version)
-            list(GET NOS_SDK_DIRS ${found_version_idx} nos_sdk_dir)
-            set(${out_sdk_dir} ${nos_sdk_dir})
-            string(REPLACE "." "_" version_target_suffix ${found_version})
-            set(${out_nos_plugin_sdk} nosPluginSDK_${version_target_suffix})
-            set(${out_nos_subsystem_sdk} nosSubsystemSDK_${version_target_suffix})
-        endif()
+        if(NOT found_version_idx EQUAL -1)
+            nos_fatal_error("No compatible version found.")
+		endif()
+		list(GET NOS_VERSIONS ${found_version_idx} found_version)
+		list(GET NOS_SDK_DIRS ${found_version_idx} nos_sdk_dir)
+		set(${out_sdk_dir} ${nos_sdk_dir})
+		string(REPLACE "." "_" version_target_suffix ${found_version})
+		set(${out_nos_plugin_sdk} nosPluginSDK_${version_target_suffix})
+		set(${out_nos_subsystem_sdk} nosSubsystemSDK_${version_target_suffix})
     endif()
 
     list(GET NOS_VERSIONS ${found_version_idx} found_version)
     list(GET NOS_SDK_DIRS ${found_version_idx} nos_sdk_dir)
+	list(GET NOS_PLUGIN_SDK_VERSIONS ${found_version_idx} found_plugin_sdk_version)
+	list(GET NOS_SUBSYSTEM_SDK_VERSIONS ${found_version_idx} found_subsystem_sdk_version)
     message(STATUS "Using Nodos version ${found_version}")
     set(${out_sdk_dir} ${nos_sdk_dir})
-    string(REPLACE "." "_" version_target_suffix ${found_version})
-    set(${out_nos_plugin_sdk} nosPluginSDK_${version_target_suffix})
-    set(${out_nos_subsystem_sdk} nosSubsystemSDK_${version_target_suffix})
+	string(REPLACE "." "_" plugin_sdk_version_target_suffix "${found_plugin_sdk_version}")
+	string(REPLACE "." "_" subsystem_sdk_target_suffix "${found_subsystem_sdk_version}")
+    set(${out_nos_plugin_sdk} nosPluginSDK_${plugin_sdk_version_target_suffix})
+    set(${out_nos_subsystem_sdk} nosSubsystemSDK_${subsystem_sdk_target_suffix})
+endmacro()
+
+macro(nos_find_plugin_sdk requested_plugin_sdk_version out_nos_plugin_sdk out_sdk_dir)
+# For all nodos sdks, call `nodos sdk-info ${requested_plugin_sdk_version} plugin` and read the json if ret code is 0
+	if(NOT NOSMAN_EXECUTABLE)
+		nos_fatal_error("Unable to find nosman. Set NOSMAN_EXECUTABLE to use nos_find_plugin_sdk.")
+	endif()
+	execute_process(
+		COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NOSMAN_WORKSPACE_DIR}" sdk-info ${requested_plugin_sdk_version} plugin
+		RESULT_VARIABLE result_code
+		OUTPUT_VARIABLE sdk_info_json
+		ERROR_QUIET
+	)
+	if (NOT result_code EQUAL 0)
+		nos_fatal_error("Unable to find plugin SDK version ${requested_plugin_sdk_version}.")
+	endif()
+	# Parse the JSON output to extract the SDK directory
+	string(JSON sdk_plugin_version  ERROR_VARIABLE err GET "${sdk_info_json}" "version")
+	if (NOT err STREQUAL "NOTFOUND")
+		message(FATAL_ERROR "Unable to parse JSON output: ${err}")
+	endif()
+	string(JSON sdk_path  ERROR_VARIABLE err GET "${sdk_info_json}" "path")
+	if (NOT err STREQUAL "NOTFOUND")
+		message(FATAL_ERROR "Unable to parse JSON output: ${err}")
+	endif()
+
+	message(STATUS "Using Nodos plugin SDK version ${sdk_plugin_version}")
+
+	string(REPLACE "." "_" sdk_plugin_version_target_suffix "${sdk_plugin_version}")
+
+	set(${out_nos_plugin_sdk} nosPluginSDK_${sdk_plugin_version_target_suffix})
+	set(${out_sdk_dir} ${sdk_path})
+
 endmacro()
