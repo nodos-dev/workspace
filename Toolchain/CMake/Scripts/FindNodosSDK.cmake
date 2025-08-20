bba70fe114e6fc0a2c12caddcd0cc58c569e3440
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2025-08-20 13:24:06 +0300

    Get nodos.sdk.plugin for newer SDKs

--- Toolchain/CMake/Scripts/FindNodosSDK.cmake
+++ Toolchain/CMake/Scripts/FindNodosSDK.cmake
@@ -84,40 +84,47 @@ macro(nos_find_sdk requested_version out_nos_plugin_sdk out_nos_subsystem_sdk ou
 	endif()
 endmacro()
 
-macro(nos_find_plugin_sdk requested_plugin_sdk_version out_nos_plugin_sdk out_sdk_dir)
-# For all nodos sdks, call `nodos sdk-info ${requested_plugin_sdk_version} plugin` and read the json if ret code is 0
-	if(NOT NOSMAN_EXECUTABLE)
-		nos_fatal_error("Unable to find nosman. Set NOSMAN_EXECUTABLE to use nos_find_plugin_sdk.")
-	endif()
-	execute_process(
-		COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NOSMAN_WORKSPACE_DIR}" sdk-info ${requested_plugin_sdk_version} plugin
-		RESULT_VARIABLE result_code
-		OUTPUT_VARIABLE sdk_info_json
-	)
-	if (NOT result_code EQUAL 0)
-		nos_fatal_error("Unable to find compatible Plugin SDK version for requested version ${requested_plugin_sdk_version}.")
-	endif()
-
-	# Parse the JSON output to extract the SDK directory
-	string(JSON plugin_sdk_version  ERROR_VARIABLE err GET "${sdk_info_json}" "version")
-	if (NOT err STREQUAL "NOTFOUND")
-		message(FATAL_ERROR "Unable to parse JSON output: ${err}")
-	endif()
-	string(JSON sdk_path  ERROR_VARIABLE err GET "${sdk_info_json}" "path")
-	if (NOT err STREQUAL "NOTFOUND")
-		message(FATAL_ERROR "Unable to parse JSON output: ${err}")
-	endif()
-
-	message(STATUS "Using Nodos Plugin SDK version ${plugin_sdk_version}")
-
-	string(REPLACE "." "_" plugin_sdk_version_target_suffix "${plugin_sdk_version}")
-
-	set(${out_nos_plugin_sdk} nosPluginSDK_${plugin_sdk_version_target_suffix})
-	set(${out_sdk_dir} ${sdk_path})
-
-	if (sdk_plugin_version VERSION_GREATER_EQUAL "39.11.0")
-		set(FLATC_EXECUTABLE "${nos_sdk_dir}/Plugin/Binaries/flatc" CACHE PATH "Path to the flatc executable" FORCE)
+macro(nos_find_plugin_sdk requested_sdk_version out_sdk_target out_sdk_dir)
+	if (requested_sdk_version VERSION_GREATER_EQUAL "39.11.0")
+		nos_get_package("nodos.sdk.plugin" ${requested_sdk_version} sdk_target_name)
+		if (NOT TARGET sdk_target_name)
+			nos_fatal_error("Failed to find Nodos Plugin SDK ${requested_sdk_version}.")
+		endif()
+		nos_get_package_info("nodos.sdk.plugin" ${requested_sdk_version} sdk_path)
+		set(${out_sdk_target} ${sdk_target_name})
+		set(${out_sdk_dir} ${sdk_path})
+		set(FLATC_EXECUTABLE "${sdk_path}/Binaries/flatc" CACHE PATH "Path to the flatc executable" FORCE)
 	else()
+		# For all nodos sdks, call `nodos sdk-info ${requested_sdk_version} plugin` and read the json if ret code is 0
+		if(NOT NOSMAN_EXECUTABLE)
+			nos_fatal_error("Unable to find nosman. Set NOSMAN_EXECUTABLE to use nos_find_plugin_sdk.")
+		endif()
+		execute_process(
+			COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NOSMAN_WORKSPACE_DIR}" sdk-info ${requested_sdk_version} plugin
+			RESULT_VARIABLE result_code
+			OUTPUT_VARIABLE sdk_info_json
+		)
+		if (NOT result_code EQUAL 0)
+			nos_fatal_error("Unable to find compatible Plugin SDK version for requested version ${requested_sdk_version}.")
+		endif()
+
+		# Parse the JSON output to extract the SDK directory
+		string(JSON plugin_sdk_version  ERROR_VARIABLE err GET "${sdk_info_json}" "version")
+		if (NOT err STREQUAL "NOTFOUND")
+			message(FATAL_ERROR "Unable to parse JSON output: ${err}")
+		endif()
+		string(JSON sdk_path  ERROR_VARIABLE err GET "${sdk_info_json}" "path")
+		if (NOT err STREQUAL "NOTFOUND")
+			message(FATAL_ERROR "Unable to parse JSON output: ${err}")
+		endif()
+
+		message(STATUS "Using Nodos Plugin SDK version ${plugin_sdk_version}")
+
+		string(REPLACE "." "_" plugin_sdk_version_target_suffix "${plugin_sdk_version}")
+
+		set(${out_sdk_target} nosPluginSDK_${plugin_sdk_version_target_suffix})
+		set(${out_sdk_dir} ${sdk_path})
+
 		set(FLATC_EXECUTABLE "${nos_sdk_dir}/bin/flatc" CACHE PATH "Path to the flatc executable" FORCE)
 	endif()
 endmacro()
