1733582785159a51bf9d5d96f795ee9897ba4793
Caner <caner.coskun@nodos.dev>
2025-12-19 18:43:16 +0300

    Use new nosman cli argument structure for info based on manifest path

--- Toolchain/CMake/Scripts/Projects.cmake
+++ Toolchain/CMake/Scripts/Projects.cmake
@@ -421,7 +421,7 @@ endmacro()
 
 function(nos_get_package_info_by_path path out_name out_version out_json)
 	execute_process(
-		COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NOSMAN_WORKSPACE_DIR}" info "" "" ${path}
+		COMMAND ${NOSMAN_EXECUTABLE} --workspace "${NOSMAN_WORKSPACE_DIR}" info "--manifest" ${path}
 		RESULT_VARIABLE nosman_result
 		OUTPUT_VARIABLE nosman_output
 	)
