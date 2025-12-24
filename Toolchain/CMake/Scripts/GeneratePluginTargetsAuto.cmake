6ac92f7a2b1395fe143d11b82755ca871fed5c50
Furkan Can Turan <furkancan.turan@nodos.dev>
2025-12-24 15:25:38 +0300

    Search for 'b' letter in plugin version

--- Toolchain/CMake/Scripts/GeneratePluginTargetsAuto.cmake
+++ Toolchain/CMake/Scripts/GeneratePluginTargetsAuto.cmake
@@ -100,8 +100,17 @@ function(_nos_generate_plugin_target plugin_manifest_file_path plugin_name manif
 	endif()
 endfunction()
 
-function(_nos_configure_plugin plugin_manifest_file common_dependencies common_definitions)
+function(_nos_configure_plugin dir plugin_manifest_file common_dependencies common_definitions)
+	if(NOT IS_DIRECTORY ${dir})
+		nos_colored_message(COLOR RED "Can't process plugin because it's not folder: ${dir}")
+		return()
+	endif()
+
 	nos_get_package_info_by_path(${plugin_manifest_file} plugin_name plugin_version manifest_json)
+	if("${plugin_version}" MATCHES "b")
+		return()
+	endif()
+	
 	nos_colored_message(COLOR CYAN "Configuring ${plugin_name} (${plugin_version})")
 
 	set(old_cmake_source_dir ${CMAKE_CURRENT_SOURCE_DIR})
@@ -167,7 +176,7 @@ function(_nos_process_plugin_directories_recursive dir common_dependencies commo
 		list(GET PLUGINS 0 plugin_manifest_filepath)
 		get_filename_component(plugin_name "${plugin_manifest_filepath}" NAME)
 		
-		_nos_configure_plugin(${plugin_manifest_filepath} "${common_dependencies}" "${common_definitions}")
+		_nos_configure_plugin(${dir} ${plugin_manifest_filepath} "${common_dependencies}" "${common_definitions}")
 	endif()
 
 	file(GLOB SUBDIRS RELATIVE ${dir} ${dir}/*)
