402c78e197ae8ac65a41bd6157a9980b15f7c219
Furkan Can TURAN <fcturan10@gmail.com>
2026-02-24 15:19:11 +0300

    idk

--- Toolchain/nosman/src/nosman/command/publish.rs
+++ Toolchain/nosman/src/nosman/command/publish.rs
@@ -18,7 +18,7 @@ use tempfile::{tempdir};
 use zip::write::{SimpleFileOptions};
 use chrono::{Utc};
 use globwalk::{GlobWalkerBuilder};
-use crate::nosman::command::{get_version_check_arg, Command, CommandError, CommandResult};
+use crate::nosman::command::{Command, CommandError, CommandResult, get_version_check_arg};
 use crate::nosman::command::CommandError::{Runtime, InvalidArgument};
 use crate::nosman::{common, constants};
 use crate::nosman::index::{PackageReleaseEntry, PackageType, SemVer, VersionCheckStrategy};
@@ -242,10 +242,9 @@ impl PublishCommand {
                     if sdk_version.is_some(){
                         api_version_opt = sdk_version.as_ref().and_then(|s| SemVer::parse_from_str(s.as_str()));
 
-                        let nodos_ver = Some(SemVer::new(1, Some(4), Some(0), None));
                         if let Some(local_package) = workspace.get_package(name.as_ref().unwrap(), version.as_ref().unwrap()).cloned() {  
                             if let Ok(plugin) = PluginEntry::new(local_package) {  
-                                plugin.get_node_definitions(&nodos_ver).iter().for_each(|node_def| {  
+                                plugin.get_node_definitions(true).iter().for_each(|node_def| {  
                                     node_class_names.push(node_def.class_name.clone());  
                                 });  
                             }  
@@ -269,6 +268,7 @@ impl PublishCommand {
         }
 
         println!("Target platform: {:?}", target_platform);
+        println!("Node class names: {:?}", node_class_names);
 
         let name = name.unwrap();
         let version = version.unwrap() + version_suffix;
@@ -409,7 +409,8 @@ impl PublishCommand {
             module_tags: package_tags,
             release_tags: if release_tags.is_empty() { None } else { Some(release_tags.clone()) },
             platform: Some(target_platform.to_string()),
-            node_names: if node_class_names.is_empty() { None } else { Some(node_class_names) }          };
+            node_names: if node_class_names.is_empty() { None } else { Some(node_class_names) }
+        };
         if verbose {
             println!("Release entry: {:?}", release);
         }
--- Toolchain/nosman/src/nosman/plugin.rs
+++ Toolchain/nosman/src/nosman/plugin.rs
@@ -38,27 +38,25 @@ impl PluginEntry {
         Ok(PluginEntry { package })
     }
 
-    pub fn get_node_definitions(&self, nodos_version: &Option<SemVer>) -> Vec<NodeDefinition> {
+    pub fn get_node_definitions(&self, include_nodes_folder: bool) -> Vec<NodeDefinition> {
         // Read module manifest file as JSON, and read node definition files
         let manifest_json = self.package.read_manifest();
         let node_defs_rel_paths_opt = manifest_json["node_definitions"].as_array();
         let mut node_defs_rel_paths = vec![];
         let mut node_definitions = vec![];
 
-        // If nodos version is 1.4+, add .nosnode files in Nodes folder.
-        if let Some(nodos_version) = nodos_version {
-            if *nodos_version >= NODOS_1_4 {
-                // Find .nosnode files under Nodes/ folder
-                let nodes_dir = self.package.get_package_root().join("Nodes");
-                if nodes_dir.exists() {
-                    for entry in fs::read_dir(&nodes_dir).unwrap_or_else(|e| panic!("Failed to read Nodes directory {:?}: {}", nodes_dir, e)) {
-                        let entry = entry.unwrap();
-                        let path = entry.path();
-                        if path.is_file() && path.extension().map_or(false, |ext| ext == constants::NODE_DEFINITION_FILE_EXT) {
-                            let rel_path = get_rel_path_based_on(&path.canonicalize().unwrap(), &self.package.get_package_root());
-                            let rel_path_str = rel_path.to_string_lossy().to_string();
-                            node_defs_rel_paths.push(rel_path_str);
-                        }
+        // If include_nodes_folder is true, add .nosnode files in Nodes folder.
+        if include_nodes_folder {
+            // Find .nosnode files under Nodes/ folder
+            let nodes_dir = self.package.get_package_root().join("Nodes");
+            if nodes_dir.exists() {
+                for entry in fs::read_dir(&nodes_dir).unwrap_or_else(|e| panic!("Failed to read Nodes directory {:?}: {}", nodes_dir, e)) {
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    if path.is_file() && path.extension().map_or(false, |ext| ext == constants::NODE_DEFINITION_FILE_EXT) {
+                        let rel_path = get_rel_path_based_on(&path.canonicalize().unwrap(), &self.package.get_package_root());
+                        let rel_path_str = rel_path.to_string_lossy().to_string();
+                        node_defs_rel_paths.push(rel_path_str);
                     }
                 }
             }
@@ -95,7 +93,7 @@ impl PluginEntry {
                     class_name: curr_class_name.clone(),
                     defined_in: node_defs_path.clone(),
                     index,
-                    json: node_json.clone(),
+                    json: node_defs.clone(),  
                     owner: self.package.clone(),
                 });
             }
@@ -103,8 +101,11 @@ impl PluginEntry {
         node_definitions
     }
 
+    // Iterates over all node definitions and finds the one with the given class name.
+    // Don't call this function multiple times if you have many nodes, as it will read and parse node definition files every time. Instead, call get_node_definitions once and find the node definition from the returned list.
     pub fn get_node_definition(&self, class_name: &str, nodos_version: &Option<SemVer>) -> Option<NodeDefinition>{
-        let node_defs = self.get_node_definitions(nodos_version);
+        let include_nodes_folder = nodos_version.as_ref().map_or(false, |v| v >= &NODOS_1_4);
+        let node_defs = self.get_node_definitions(include_nodes_folder);
         return node_defs.into_iter().find(|def| def.class_name == class_name);
     }
 
